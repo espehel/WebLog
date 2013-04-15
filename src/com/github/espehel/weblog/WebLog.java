fa8f8e745c66f3afd80976ca24ddb149ae7a048a
Espen Hellerud <espenhellerud5@gmail.com>
2013-04-15 14:47:54 +0200

    created onEnable()/Disable()

--- src/com/github/espehel/weblog/WebLog.java
+++ src/com/github/espehel/weblog/WebLog.java
@@ -3,5 +3,14 @@ package com.github.espehel.weblog;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class WebLog extends JavaPlugin {
+	@Override
+	public void onEnable(){
+		//TODO insert code for when the plugin is enable. Sets up the plugin
+	}
+	
+	@Override
+	public void onDisable(){
+		//TODO insert code for when the plugin is disabled. Clean up stuff
+	}
 
 }
