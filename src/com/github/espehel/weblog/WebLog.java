db47be2f3bebb6835ebdb8681238efa6594dfda6
Espen Hellerud <espenhellerud5@gmail.com>
2013-04-15 14:59:58 +0200

    removed lines

--- src/com/github/espehel/weblog/WebLog.java
+++ src/com/github/espehel/weblog/WebLog.java
@@ -6,11 +6,9 @@ public final class WebLog extends JavaPlugin {
 	@Override
 	public void onEnable(){
 		//TODO insert code for when the plugin is enable. Sets up the plugin
-	}
-	
+	}	
 	@Override
 	public void onDisable(){
 		//TODO insert code for when the plugin is disabled. Clean up stuff
 	}
-
 }
