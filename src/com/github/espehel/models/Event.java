5978aac8d2b76700bffc021398cd58e8272a4df8
Espen Hellerud <espenhellerud5@gmail.com>
2013-04-19 15:05:15 +0200

    Finished event model

--- src/com/github/espehel/models/Event.java
+++ src/com/github/espehel/models/Event.java
@@ -1,5 +1,28 @@
 package com.github.espehel.models;
 
-public class Event {
+import java.util.GregorianCalendar;
 
+public class Event {
+	private int id;
+	private GregorianCalendar datetime;
+	private String name;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public GregorianCalendar getDatetime() {
+		return datetime;
+	}
+	public void setDatetime(GregorianCalendar datetime) {
+		this.datetime = datetime;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
 }
