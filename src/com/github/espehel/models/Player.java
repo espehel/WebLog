441cf516b336744f94d4d73b958397af9a5b2244
Espen Hellerud <espenhellerud5@gmail.com>
2013-04-19 15:05:35 +0200

    finished player model

--- src/com/github/espehel/models/Player.java
+++ src/com/github/espehel/models/Player.java
@@ -1,5 +1,19 @@
 package com.github.espehel.models;
 
 public class Player {
-
+	private int id;
+	private String nick;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNick() {
+		return nick;
+	}
+	public void setNick(String nick) {
+		this.nick = nick;
+	}
 }
